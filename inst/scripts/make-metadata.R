c5b1e71ba5d9ef9b3c719e7b251b711d2bc3823d
Lukas Weber <lukmweber@gmail.com>
2018-03-31 20:43:48 +0200

    fix metadata

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -8,12 +8,18 @@ df_Bodenmiller_BCR_XL_SE <- data.frame(
   Title = "Bodenmiller_BCR_XL_SE", 
   Description = Bodenmiller_BCR_XL_SE_desc, 
   BiocVersion = "3.7", 
+  Genome = NA, 
   SourceType = "FCS", 
+  SourceUrl = "http://imlspenticton.uzh.ch/robinson_lab/HDCytoData/", 
+  SourceVersion = NA, 
   Species = "Homo sapiens", 
   TaxonomyId = "9606", 
+  Coordinate_1_based = TRUE, 
+  DataProvider = "", 
+  Maintainer = "Lukas M. Weber <lukmweber@gmail.com>", 
   RDataClass = "SummarizedExperiment", 
-  RDataPath = "", 
   DispatchClass = "Rda", 
+  RDataPath = NA, 
   stringsAsFactors = FALSE
 )
 
